ccdeb94bcf5248b795ba7d254a4a872a3e2acd5d
KL <ihearsay@gmail.com>
2021-07-13 17:52:53 +0800

    [MISC] Add some string sorting.

--- src/cc/umeet/Main.java
+++ src/cc/umeet/Main.java
@@ -7,6 +7,13 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         int[][] arrays = {{1, 2, 3, 4}, {3, 6, 7, 2}};
-        System.err.println(Arrays.deepToString(arrays));
+        System.out.println(Arrays.deepToString(arrays));
+        String[] strings = {"Name", "Gender"};
+        System.out.println(Arrays.deepToString(strings));
+        Arrays.sort(strings);
+        System.out.println(Arrays.deepToString(strings));
+
+
     }
+
 }
