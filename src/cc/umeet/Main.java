2b7b3ce3d91479b1a3e1be7cff792b2f9d17c4cd
ihearsay <hkl679@163.com>
2021-07-13 17:51:49 +0800

    Add some comments

--- src/cc/umeet/Main.java
+++ src/cc/umeet/Main.java
@@ -8,5 +8,6 @@ public class Main {
 	// write your code here
         int[][] arrays = {{1, 2, 3, 4}, {3, 6, 7, 2}};
         System.err.println(Arrays.deepToString(arrays));
+	// doing some tests here
     }
 }
