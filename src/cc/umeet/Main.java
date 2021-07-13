78ebe6c188eec1d06663e5438c4d87bd40c47bfa
KL <ihearsay@gmail.com>
2021-07-13 17:36:44 +0800

    [MISC] First commit

---
+++ src/cc/umeet/Main.java
@@ -0,0 +1,12 @@
+package cc.umeet;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int[][] arrays = {{1, 2, 3, 4}, {3, 6, 7, 2}};
+        System.err.println(Arrays.deepToString(arrays));
+    }
+}
