292644105aa65832fd414291d41fdfe7631b859b
ihearsay <hkl679@163.com>
2021-07-13 18:07:38 +0800

    Add more comment

--- src/cc/umeet/Main.java
+++ src/cc/umeet/Main.java
@@ -9,6 +9,7 @@ public class Main {
         int[][] arrays = {{1, 2, 3, 4}, {3, 6, 7, 2}};
         System.err.println(Arrays.deepToString(arrays));
 	// doing some tests here
+	// add more comment
         System.out.println(Arrays.deepToString(arrays));
         String[] strings = {"Name", "Gender"};
         System.out.println(Arrays.deepToString(strings));
